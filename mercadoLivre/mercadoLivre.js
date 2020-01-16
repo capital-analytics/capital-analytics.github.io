2429162af267177d453b2b50468b122eeebc9bb8
Ana Julia <anajuliapipiu@gmail.com>
2020-01-15 21:18:56 -0300

    Implementação de dimensões e melhorias

--- mercadoLivre/mercadoLivre.js
+++ mercadoLivre/mercadoLivre.js
@@ -1,11 +1,11 @@
 //https://medium.com/@oieduardorabelo/javascript-armadilhas-do-asyn-await-em-loops-1cdad44db7f0
-async function getDataML(len) {
+async function getDataML(category, len) {
     var ofsset = 0;
     var urls = [];
 
     while (ofsset <= len) {
         if (ofsset % 50 == 0) {
-            urls.push('https://api.mercadolibre.com/sites/MLB/search?category=MLB194799&offset=' + ofsset);
+            urls.push(`https://api.mercadolibre.com/sites/MLB/search?category=${category}&offset=${ofsset}`);
         }
         ofsset++;
     }
@@ -13,9 +13,26 @@ async function getDataML(len) {
     var data = [];
     for (const url of urls) {
         //await fetch(url).then(t=>{console.log(t)});
-        await fetch(url).then(t=>{t.json().then(j=>{for (const o of j.results){data.push(o);}})});
+        await fetch(url).then(t=>{
+            t.json().then(j=>{
+                for (const o of j.results){
+                    data.push(o)
+                }
+            })
+        });
     }
 
     return data
 
+}
+
+
+
+//You can then use .group(fakeGroup) to properly chart your data.
+function getTops(source_group) {
+    return {
+        all: function () {
+            return source_group.top(10);
+        }
+    };
 }
