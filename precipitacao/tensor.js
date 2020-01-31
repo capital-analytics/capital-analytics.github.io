09863d79ccdeb2b61f4a0ff0b1bee514195f55fb
Ana Julia <anajuliapipiu@gmail.com>
2020-01-31 00:23:53 -0300

    Implementação da versão inicial do tensor

--- precipitacao/tensor.js
+++ precipitacao/tensor.js
@@ -26,7 +26,9 @@ async function loadData() {
                   vento_vel:         d.vento_vel,
                   vento_dir:         d.vento_direcao,
                   precipitacao:      d.precipitacao
-            }));  
+            })).filter(f => {
+                return f.temp_max !== "////"
+            })
 
             dados.forEach(f => {
                 data.push(f);
@@ -38,8 +40,27 @@ async function loadData() {
 
 async function run() {
     // Load and plot the original input data that we are going to train on.
-    loadData().then(data => {
-        console.log(data);
+    await loadData().then(data => {
+        console.log(data.length);
+
+
+        const model = tf.sequential();
+        model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+        model.compile({
+            loss: 'meanSquaredError',
+            optimizer: 'sgd'
+        })
+
+        const xs = tf.tensor2d([1,-1,3,5,8,9,7,10], [8,1]);
+        const ys = tf.tensor2d([10,-11,30,50,80,90,70,100], [8,1]);
+
+
+        model.fit(xs, ys, {epochs: 500});
+
+        let predict = model.predict(tf.tensor2d([35], [1, 1]));
+
+        d3.select('#tensor').append('div').text(predict);
     });
 
 
